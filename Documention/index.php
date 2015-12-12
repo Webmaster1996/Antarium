58b51b5e220db6e0d89a98a04b66a079bac4b3f9
Danter14 <skype danter1400>
2015-12-12 15:20:50 +0100

    Addon
    Ajout de la futur page de la documentation pour vous aider lors de
    modification ou changement

---
+++ Documention/index.php
@@ -0,0 +1,15 @@
+﻿<?php
+/**
+ * Projet : Antarium
+ * Copyright (C) 2015 Danter14
+ *
+ * Ce projet est totalement en open source il peux donc être
+ * modifier et redistribuer gratuitement.
+ *
+ * @package Antarium
+ * @author Danter14
+ * @copyright 2015 Danter14
+ * @license GNU GENERAL PUBLIC LICENSE
+ * @version 1.0 (12/12/2015)
+ * @info Fichier: index.php / réservé à toute la documentation
+ */
